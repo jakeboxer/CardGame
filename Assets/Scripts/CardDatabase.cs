2c5568f8e4330500fb1de7efc42f246c600b9d5c
Jake Boxer <jake@github.com>
2017-04-26 00:31:42 -0700

    Add the rest of the initial project

---
+++ Assets/Scripts/CardDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class CardDatabase : MonoBehaviour {
+}
+
+public class CardModel {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Attack { get; set; }
+
+    public CardModel() {
+        Id = -1;
+    }
+
+    public CardModel(JsonData data) {
+        Id = (int)data["id"];
+        Title = (string)data["title"];
+        Attack = (int)data["attack"];
+    }
+}
