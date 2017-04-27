448816f477e9692d1d5c96d251e89512a6bccf79
Jake Boxer <jake@github.com>
2017-04-27 00:22:19 -0700

    Dynamically load cards

--- Assets/Scripts/CardDatabase.cs
+++ Assets/Scripts/CardDatabase.cs
@@ -1,15 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using LitJson;
+using System.IO;
 
 public class CardDatabase : MonoBehaviour {
+	private Dictionary<int, CardModel> cardsById = new Dictionary<int, CardModel>();
+
+	void Start() {
+		string jsonPath = Application.dataPath + "/StreamingAssets/cards.json";
+		JsonData cardJson = JsonMapper.ToObject(File.ReadAllText(jsonPath));
+
+		foreach(JsonData cardData in cardJson) {
+			CardModel cardModel = new CardModel(cardData);
+			cardsById.Add(cardModel.Id, cardModel);
+		}
+	}
+
+	public CardModel FindCardModel(int id) {
+		return cardsById[id];
+	}
+
+	public ICollection<CardModel> GetAllCardModels() {
+		return cardsById.Values;
+	}
 }
 
 public class CardModel {
     public int Id { get; set; }
     public string Title { get; set; }
-    public int Attack { get; set; }
+	public string Description { get; set; }
+	public int EnergyCost { get; set; }
+	public int Attack { get; set; }
+	public int Defense { get; set; }
+	public Sprite Sprite { get; set; }
 
     public CardModel() {
         Id = -1;
@@ -18,6 +43,19 @@ public class CardModel {
     public CardModel(JsonData data) {
         Id = (int)data["id"];
         Title = (string)data["title"];
-        Attack = (int)data["attack"];
+		Description = (string)data["description"];
+		EnergyCost = (int)data["energy_cost"];
+
+		if(data.Keys.Contains("attack")) {
+			Attack = (int)data["attack"];
+		}
+
+		if(data.Keys.Contains("defense")) {
+			Defense = (int)data["defense"];
+		}
+
+		string spriteFilename = (string)data["sprite_filename"];
+		Sprite[] itemSprites = Resources.LoadAll<Sprite>("Sprites/CardArt/roguelike_items");
+		Sprite = itemSprites.Single(s => s.name == spriteFilename);
     }
 }
---
+++ Assets/Scripts/CardPresenter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardPresenter : MonoBehaviour {
+	public GameObject cardPrefab;
+	private CardDatabase cardDatabase;
+	
+	void Start () {
+		cardDatabase = GetComponent<CardDatabase>();
+		Debug.Log(cardDatabase.GetAllCardModels().Count);
+
+		foreach(CardModel cardModel in cardDatabase.GetAllCardModels()) {
+			GameObject cardGameObject = Instantiate(cardPrefab);
+			Transform cardTransform = cardGameObject.transform;
+			cardTransform.SetParent(transform);
+
+			cardTransform.FindChild("Title").GetComponent<Text>().text = cardModel.Title;
+			cardTransform.FindChild("Description").GetComponent<Text>().text = cardModel.Description;
+			cardTransform.FindChild("Energy Container").FindChild("Text").GetComponent<Text>().text = cardModel.EnergyCost.ToString();
+			cardTransform.FindChild("Art").GetComponent<Image>().sprite = cardModel.Sprite;
+		}
+	}
+}
